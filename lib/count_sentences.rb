4e63ae87f3610343fa8ee8679db0832278a4633e
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-02 15:43:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,41 @@ class String
 
   def sentence?
     
+    if self.end_with?('.')
+      return true 
+    else 
+      return false 
+    end
+    
   end
 
   def question?
+    
+    if self.end_with?('?')
+      return true 
+    else 
+      return false 
+    end
 
   end
 
   def exclamation?
+    
+    if self.end_with?('!')
+      return true 
+    else 
+      return false 
+    end
 
   end
 
   def count_sentences
+    
+   splut = self.split()    
+    binding.pry 
 
   end
 end
+
+
+"Hello".count_sentences
